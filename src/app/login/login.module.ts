449c24766f25aae4d7dc3723e3f05c907f06d7e3
sdbonilla <santiagoboilla@unicomfacauca.edu.co>
2021-03-18 17:07:02 -0500

    Continue

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -15,6 +15,7 @@ import { LoginPage } from './login.page';
     IonicModule,
     LoginPageRoutingModule
   ],
-  declarations: [LoginPage]
+  declarations: [LoginPage],
+  exports: [LoginPage]
 })
 export class LoginPageModule {}
