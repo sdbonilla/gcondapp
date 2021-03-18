1a3176d07c5feb3012124de838ae6eef927c933c
sdbonilla <santiagoboilla@unicomfacauca.edu.co>
2021-03-18 11:59:56 -0500

    Iniciando

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,92 @@
+import { Injectable } from '@angular/core';
+import { User } from '../shared/user.interface';
+import { AngularFireAuth } from "@angular/fire/auth";
+import * as firebase from 'firebase/app';
+
+import { AngularFirestore, AngularFirestoreDocument } from "@angular/fire/firestore";
+import { Observable, of } from 'rxjs';
+import { switchMap } from "rxjs/operators";
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  user$:Observable<User>;
+
+  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore) {
+    this.user$ = this.afAuth.authState.pipe(
+      switchMap((user)=>{
+        if (user){
+          return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+        }
+        return of(null);
+      })
+    );
+   }
+  
+
+  async loginGoogle():Promise<User>{
+
+    try {
+      const {user} = await this.afAuth.signInWithPopup(new firebase.default.auth.GoogleAuthProvider());
+      this.updateUserdata(user);
+      return user;
+    } catch (error) {
+      console.log('Error->',error)
+    }
+  }
+  async sendVerificationEmail():Promise<void>{
+
+    try {
+      return (await this.afAuth.currentUser).sendEmailVerification();
+    } catch (error) {
+      console.log('Error->',error)
+    }
+  }
+  async resetPassw(email: string):Promise<void>{
+    try {
+      return this.afAuth.sendPasswordResetEmail(email)
+    } catch (error) {
+      
+    }
+  }
+  async login(email: string, password: string): Promise<User>{
+    try {
+      const { user }= await this.afAuth.signInWithEmailAndPassword(email, password);
+      this.updateUserdata(user);
+      return user;
+
+    } catch (error) {
+      console.log('error->',error);
+    }
+  }
+  async register(email: string, password: string): Promise<User> {
+
+    try {
+      const{ user} = await this.afAuth.createUserWithEmailAndPassword(email, password);
+      await this.sendVerificationEmail();
+      return user;
+     } catch (error) {
+      
+    }
+  }
+  async logout():Promise<void> {
+    try {
+      await this.afAuth.signOut();
+    } catch (error) {
+      console.log('error->',error);   
+    }
+  }
+
+  private updateUserdata(user:User){
+    const userRef:AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
+    const data:User = { 
+      uid:user.uid,
+      email:user.email,
+      emailVerified: user.emailVerified,
+      displayName: user.displayName,
+    };
+
+    return userRef.set(data, {merge: true});
+  }
+  
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBrdzHQTH-mX-RABOQbC-hvhbRds6X8H64",
+    authDomain: "gcond-f44b4.firebaseapp.com",
+    projectId: "gcond-f44b4",
+    storageBucket: "gcond-f44b4.appspot.com",
+    messagingSenderId: "329714527463",
+    appId: "1:329714527463:web:677be730609c6c5a298642"
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
