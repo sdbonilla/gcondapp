9d8446ab8a7be5f996303de7b1fa5afbbaeb49b8
sdbonilla <santiagoboilla@unicomfacauca.edu.co>
2021-03-21 22:17:54 -0500

    fiinginterfce

--- src/app/gestiontorres/gestiontorres.page.ts
+++ src/app/gestiontorres/gestiontorres.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { EdificeI } from "../shared/edifice.interface";
 import { CrudedificeService } from "../service/crudedifice.service";
+import { pisoI } from '../shared/pisos.interface';
+import { apartI } from '../shared/apartamento.interface';
+import { CrudpisosService } from '../service/crudpisos.service';
+import { CrudapartamentoService } from '../service/crudapartamento.service';
 @Component({
   selector: 'app-gestiontorres',
   templateUrl: './gestiontorres.page.html',
@@ -9,11 +13,15 @@ import { CrudedificeService } from "../service/crudedifice.service";
 export class GestiontorresPage implements OnInit {
 
   torres: EdificeI[];
+  pisos: pisoI[];
+  aptos: apartI[];
 
-  constructor(private torresService: CrudedificeService) { }
+  constructor(private torresService: CrudedificeService, private pisosService: CrudpisosService, private aptosService: CrudapartamentoService) { }
 
   ngOnInit() {
     this.torresService.getTorres().subscribe(res=>this.torres = res);
+    this.pisosService.getPisos().subscribe(res=>this.pisos = res);
+    this.aptosService.getAptos().subscribe(res=>this.aptos = res);
   }
 
 }
